d7b3962811f498744e21a865590b1238fd944bf0
Daisuke Araki <daisukeArk@users.noreply.github.com>
2018-05-20 16:39:40 +0900

    remove unused file

--- test/skills/hello-world/index.ts
+++
@@ -1,9 +0,0 @@
-import * as Alexa from 'ask-sdk-core';
-import * as AlexaModel from 'ask-sdk-model';
-
-const skillBuilder = Alexa.SkillBuilders.custom();
-
-export const handler = skillBuilder
-  .addRequestHandlers()
-  .addErrorHandlers()
-  .lambda();
